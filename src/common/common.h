7a4477a040eb9436456436f389ae9376be53e29c
Daniel Ozyurt <ozyud@outlook.com>
2024-04-15 15:22:21 +0200

    Remove unused stuff

--- src/common/common.h
+++ src/common/common.h
@@ -1,17 +1,3 @@
 #pragma once
 
-#include <type_traits>
-
 #include "matrix_view.h"
-
-namespace {
-template <typename T>
-concept FloatLike = std::is_floating_point_v<T>;
-
-template <FloatLike T>
-constexpr float RemapRange(T value, T src_min, T src_max, T dst_min,
-                           T dst_max) {
-  return ((dst_max - dst_min) / (src_max - src_min)) * (value - src_min) +
-         dst_min;
-}
-}  // namespace
