129c42c47eaacd707990573f8a9c03dd47abb332
Daniel Ozyurt <ozyud@outlook.com>
2024-04-15 14:22:11 +0200

    Fix params

--- src/graphics/ray_tracer.cpp
+++ src/graphics/ray_tracer.cpp
@@ -12,9 +12,7 @@ inline float CalculateDistance(DirectX::FXMVECTOR a, DirectX::FXMVECTOR b) {
 
 // Check if the ray from intersection point to light intersects with any mesh.
 inline bool IntersectsWithShadow(const std::span<model::Mesh>& meshes,
-                                 size_t current_mesh_index,
-                                 size_t current_face_index, size_t mesh_index,
-                                 size_t face_index,
+                                 size_t mesh_index, size_t face_index,
                                  DirectX::FXMVECTOR intersection_point,
                                  const DirectX::XMFLOAT3A& light_position) {
   const auto& face = meshes[mesh_index].second[face_index];
@@ -40,7 +38,7 @@ inline bool IntersectsWithShadow(const std::span<model::Mesh>& meshes,
 // Check if a point is shadowed by geometry in the scene.
 inline bool IsShadowed(DirectX::XMVECTOR& final_color,
                        const std::span<model::Mesh>& meshes,
-                       size_t current_mesh_index, size_t current_face_index,
+                       size_t outer_mesh_index, size_t outer_face_index,
                        DirectX::FXMVECTOR intersection_point,
                        const DirectX::XMFLOAT3A& light_position) {
   constexpr float kShadowIntensity = 0.0625f;
@@ -51,17 +49,16 @@ inline bool IsShadowed(DirectX::XMVECTOR& final_color,
 
   // Iterate through meshes and faces.
   for (size_t mesh_index = 0; mesh_index < meshes.size(); ++mesh_index) {
-    if (mesh_index == current_mesh_index) continue;
+    if (mesh_index == outer_mesh_index) continue;
 
     const auto& mesh = meshes[mesh_index];
     for (size_t face_index = 0; face_index < mesh.second.size(); ++face_index) {
-      if (mesh_index == current_mesh_index && face_index == current_face_index)
+      if (mesh_index == outer_mesh_index && face_index == outer_face_index)
         continue;
 
       // Check for intersection.
-      if (IntersectsWithShadow(meshes, current_mesh_index, current_face_index,
-                               mesh_index, face_index, intersection_point,
-                               light_position)) {
+      if (IntersectsWithShadow(meshes, mesh_index, face_index,
+                               intersection_point, light_position)) {
         total_intensity += kShadowIntensity;
         // No need to check further, already in shadow.
         break;
@@ -77,17 +74,21 @@ inline bool IsShadowed(DirectX::XMVECTOR& final_color,
 
 // Check if the ray from intersection point to reflection intersects with any
 // mesh.
-inline bool IntersectsWithReflection(
-    DirectX::XMVECTOR& final_color, const std::span<model::Mesh>& meshes,
-    size_t current_mesh_index, size_t current_face_index, size_t mesh_index,
-    size_t face_index, DirectX::FXMVECTOR intersection_point,
-    DirectX::FXMVECTOR incident_direction, DirectX::FXMVECTOR surface_normal) {
+inline bool IntersectsWithReflection(DirectX::XMVECTOR& final_color,
+                                     const std::span<model::Mesh>& meshes,
+                                     size_t mesh_index, size_t face_index,
+                                     DirectX::FXMVECTOR intersection_point,
+                                     DirectX::FXMVECTOR incident_direction,
+                                     DirectX::FXMVECTOR surface_normal,
+                                     float reflectivity) {
   DirectX::XMVECTOR vertex_a{};
   DirectX::XMVECTOR vertex_b{};
   DirectX::XMVECTOR vertex_c{};
   utils::xm::triangle::Load(vertex_a, vertex_b, vertex_c,
                             meshes[mesh_index].first,
                             meshes[mesh_index].second[face_index]);
+
+  // Calculate reflection direction
   const auto reflection_direction = DirectX::XMVector3NormalizeEst(
       DirectX::XMVector3Reflect(incident_direction, surface_normal));
 
@@ -107,7 +108,6 @@ inline bool IntersectsWithReflection(
     const auto reflection_color =
         DirectX::XMVectorMultiply(barycentric_coords, final_color);
 
-    constexpr float reflectivity = 0.95f;
     final_color = DirectX::XMVectorSaturate(
         DirectX::XMVectorLerp(final_color, reflection_color, reflectivity));
     return true;
@@ -119,24 +119,30 @@ inline bool IntersectsWithReflection(
 // Trace the reflection ray and calculate reflection color.
 inline void TraceReflectionRay(DirectX::XMVECTOR& final_color,
                                const std::span<model::Mesh>& meshes,
-                               size_t current_mesh_index,
-                               size_t current_face_index,
+                               size_t outer_mesh_index, size_t outer_face_index,
                                DirectX::FXMVECTOR intersection_point,
                                DirectX::FXMVECTOR incident_direction,
                                DirectX::FXMVECTOR surface_normal) {
+  constexpr auto kReflectivity = 0.95f;
+
+  const auto reflection_direction = DirectX::XMVector3NormalizeEst(
+      DirectX::XMVector3Reflect(incident_direction, surface_normal));
+
   for (size_t mesh_index = 0; mesh_index < meshes.size(); ++mesh_index) {
-    // Skip the current mesh.
-    if (mesh_index == current_mesh_index) {
-      continue;
+    if (mesh_index == outer_mesh_index) {
+      continue;  // Skip the same mesh.
     }
 
     for (size_t face_index = 0; face_index < meshes[mesh_index].second.size();
          ++face_index) {
+      if (mesh_index == outer_mesh_index && face_index == outer_face_index) {
+        continue;  // Skip the same face.
+      }
+
       // Check for reflection intersection.
-      if (IntersectsWithReflection(final_color, meshes, current_mesh_index,
-                                   current_face_index, mesh_index, face_index,
+      if (IntersectsWithReflection(final_color, meshes, mesh_index, face_index,
                                    intersection_point, incident_direction,
-                                   surface_normal)) {
+                                   surface_normal, kReflectivity)) {
         break;
       }
     }
